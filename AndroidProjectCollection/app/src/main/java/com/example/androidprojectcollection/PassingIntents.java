b157a7f6206b3911d2d89972dec7d01bb4cdeb24
L11X08W15 <asdasdasdasd@gmail.com>
2024-04-17 10:26:45 +0800

    With Intents

--- AndroidProjectCollection/app/src/main/java/com/example/androidprojectcollection/PassingIntents.java
+++ AndroidProjectCollection/app/src/main/java/com/example/androidprojectcollection/PassingIntents.java
@@ -2,10 +2,17 @@ package com.example.androidprojectcollection;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PassingIntents extends AppCompatActivity {
+
+    TextView txtFirstname, txtLastName, txtBirthdate, txtStudId, txtBirthPlace, txtEmail, txtProgram, txtSection;
+    Button btnClear, btnSubmit;
+    RadioButton radMale, radFemale, radOthers,radSingle,radMarried,radWidowed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +21,25 @@ public class PassingIntents extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        String fName= eFName.getText().toString();
+        String firstName = txtFirstname.getText().toString();
+        String lastName = txtLastName.getText().toString();
+        String birthdate = txtBirthdate.getText().toString();
+        String studId = txtStudId.getText().toString();
+        String email = txtEmail.getText().toString();
+        String birthplace=txtBirthPlace.getText().toString();
+        String program=txtProgram.getText().toString();
+        String section=txtSection.getText().toString();
 
+
+        String gender;
+        if(radMale.isChecked())
+            gender = "Male";
+        else if (radFemale.isChecked())
+            gender = "Female";
+        else if (radOthers.isChecked())
+            gender = "Others";
+        else
+            gender = "Unknown";
     }
 
 
